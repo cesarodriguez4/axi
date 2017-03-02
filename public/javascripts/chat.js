b507017fd3b773b52492f11668a9a42f3fb4b9eb
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-03-02 19:46:41 -0400

    Creando nuevos horizontes

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,11 +1,11 @@
 var socket = io.connect('/');
 
 function sendMessage() {
-	var id_usuario = Math.floor(1e6* Math.random());
+	var id_pasajero = Math.floor(1e6* Math.random());
 	var id_transportista = Math.floor(1e6 * Math.random());
 	var mensaje = document.getElementById("mensaje").value;
 
-	var obj = {id_transportista, id_usuario, mensaje};
+	var obj = {id_transportista, id_pasajero, mensaje};
 	obj = JSON.stringify(obj)
 	console.log(obj)
 	socket.emit('nuevo_mensaje', obj );
@@ -14,6 +14,6 @@ function sendMessage() {
 socket.on("entrega_mensaje", function(data) {
 	data = JSON.parse(data);
 	var id_transportista = data.id_transportista;
-	var id_usuario = data.id_usuario;
+	var id_pasajero = data.pasajero;
 	var mensaje = data.mensaje;
 });
