c49638205bcb847792c51eca1c2404ccf48e79e5
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-03-02 21:52:45 -0400

    corregido historial

--- routes/historial.js
+++ routes/historial.js
@@ -4,9 +4,9 @@ module.exports = function(app, con) {
 	app.post("/usuarios/historial", function(req, res) {
 		var tipo = req.body.tipo;
 		var id = req.body.id;
-		var campo = 'id_pasajeros'; 
+		var campo = 'id_pasajero'; 
 		if (tipo == 1) {
-			campo = 'id_transportistas';
+			campo = 'id_transportista';
 		}
 		queries.selectWRes(res, con, 'historial', campo, id);
 	});
