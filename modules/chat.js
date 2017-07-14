2e722fd5e4b31641c20e68cf4350ce0c057344ac
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-07-13 20:16:14 -0400

    Virgen Maria

--- modules/chat.js
+++ modules/chat.js
@@ -4,6 +4,7 @@ module.exports = function(socket, connection) {
 	socket.on("nuevo_mensaje", function(msg) {
 		socket.broadcast.emit("entrega_mensaje", msg);
 		var obj = JSON.parse(msg);
+		delete obj.sender;
 		queries.insert(connection, obj, `chat`);
 	})
 }
