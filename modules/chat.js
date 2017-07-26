883fb0faa4fa8935796fc62da00e3007f66614c8
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-07-26 00:31:26 -0400

    chat

--- modules/chat.js
+++ modules/chat.js
@@ -2,7 +2,7 @@ var queries = require('./mysqli_crud');
 
 module.exports = function(socket, connection) {
 	socket.on("nuevo_mensaje", function(msg) {
-		socket.broadcast.emit("entrega_mensaje", msg);
+		socket.emit("entrega_mensaje", msg);
 		var obj = JSON.parse(msg);
 		delete obj.sender;
 		delete obj.socket;
