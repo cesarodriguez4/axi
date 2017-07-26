dfc05af05b97a4837ae7c59f0fc8d2bdf198de92
Cesar Jr Rodriguez <cesarodriguez4@gmail.com>
2017-07-26 01:41:53 -0400

    q

--- modules/chat.js
+++ modules/chat.js
@@ -4,6 +4,8 @@ module.exports = function(socket, connection) {
 	socket.on("nuevo_mensaje", function(msg) {
 		console.log('esto es lo que me llega');
 		console.log(msg);
+		msg.id_pasajero = msg.sender;
+		console.log(msg);
 		socket.emit("entrega_mensaje", msg);
 		var obj = JSON.parse(msg);
 		delete obj.sender;
